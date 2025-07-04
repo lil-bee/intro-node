7713efb23e2a5a5f4c16e458b5c96476c064ff04
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2025-07-04 13:41:04 +0700

    install yargs && implement command

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
+import "./src/command.js";
 
 const note = process.argv[2];
 const newNote = {
-	id: Date.now(),
-	note,
-}
-console.log('Welcome to CLI Notes!');
-console.log('your new note', newNote);
+  id: Date.now(),
+  note,
+};
+console.log("Welcome to CLI Notes!");
+console.log("your new note", newNote);
